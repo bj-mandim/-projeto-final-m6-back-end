89b51b1a449ae45a7e8d78726daa520eb786ca28
Gabriel Werneck <gabrielwrnck@gmail.com>
2023-04-07 11:21:11 -0300

    Created cars repositories

--- src/modules/cars/repository/cars.repository.ts
+++ src/modules/cars/repository/cars.repository.ts
@@ -1,3 +1,14 @@
-import { CreateCarDto } from "../dto/create-car.dto";
-import { UpdateCarDto } from "../dto/update-car.dto";
-import { Car } from "../entities/car.entity";
+import { CreateCarDto } from '../dto/create-car.dto';
+import { UpdateCarDto } from '../dto/update-car.dto';
+import { Car } from '../entities/car.entity';
+
+export abstract class CarRepository {
+  abstract create(data: CreateCarDto): Promise<Car> | Car | undefined;
+  abstract findAll(): Promise<Car[]> | Car[] | undefined;
+  abstract findOne(id: string): Promise<Car | undefined> | Car | undefined;
+  abstract update(
+    id: string,
+    data: UpdateCarDto,
+  ): Promise<Car> | Car | undefined;
+  abstract delete(id: string): Promise<void> | void | undefined;
+}
