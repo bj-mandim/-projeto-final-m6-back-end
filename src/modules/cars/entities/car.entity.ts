d866c3f11b3f9d1c319b4e19b1310c6a30cf709c
AmandaIsMe-alt <amanda.costa1301@hotmail.com>
2023-04-10 21:53:08 -0300

    create entities

--- src/modules/cars/entities/car.entity.ts
+++ src/modules/cars/entities/car.entity.ts
@@ -1,3 +1,59 @@
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { Comment } from './comment.entity';
+import { Image } from './image.entity';
+import { User } from './user.entity';
+@Entity('cars')
 export class Car {
-  car: boolean;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  brand: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  fuel: string;
+
+  @Column()
+  km: number;
+
+  @Column()
+  fipe_table: number;
+
+  @Column()
+  color: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  description: string;
+
+  @Column({ default: true })
+  is_active: boolean;
+
+  @OneToMany(() => Image, (image) => image.car, {
+    eager: true,
+  })
+  images: Image[];
+
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  user: User;
+
+  @OneToMany(() => Comment, (Comment) => Comment.car, {
+    onDelete: 'SET NULL',
+  })
+  comments: Comment[];
 }
