167e2b8d40485df2ea9509ac07fa99f7e5b74a89
Gabriel Werneck <gabrielwrnck@gmail.com>
2023-04-17 18:00:58 -0300

    Created users and auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { configService } from './database/postgres/config.service';
 import { CarsModule } from './modules/cars/cars.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     CarsModule,
+    UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
