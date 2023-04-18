4febf233db54fa7114a6f7b11ec249363c28c10e
MateusSantanna <mateussantanna2001@outlook.com>
2023-04-18 12:46:21 -0300

    fix: fixed bugs of string

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,34 +1,43 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
+  @IsString()
   readonly username: string;
 
   @IsNotEmpty()
+  @IsString()
   readonly email: string;
 
   @IsNotEmpty()
+  @IsString()
   readonly password: string;
 
-  @IsNotEmpty()
+  @IsBoolean()
   readonly is_announcer: boolean;
 
   @IsNotEmpty()
+  @IsString()
   readonly description: string;
 
   @IsNotEmpty()
+  @IsString()
   readonly phone: string;
 
   @IsNotEmpty()
+  @IsString()
   readonly cpf: string;
 
   @IsNotEmpty()
+  @IsString()
   readonly birth: string;
 }
 export class LoginUserDto {
   @IsNotEmpty()
+  @IsString()
   readonly email: string;
 
   @IsNotEmpty()
+  @IsString()
   readonly password: string;
 }
