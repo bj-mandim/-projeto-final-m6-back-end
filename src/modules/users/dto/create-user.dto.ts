139d735ca761122844bd7ecb4a75d9a27f800295
MateusSantanna <mateussantanna2001@outlook.com>
2023-04-18 12:24:49 -0300

    feat: user DTO and login DTO created

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,34 @@
-export class CreateUserDto {}
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  readonly username: string;
+
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsNotEmpty()
+  readonly password: string;
+
+  @IsNotEmpty()
+  readonly is_announcer: boolean;
+
+  @IsNotEmpty()
+  readonly description: string;
+
+  @IsNotEmpty()
+  readonly phone: string;
+
+  @IsNotEmpty()
+  readonly cpf: string;
+
+  @IsNotEmpty()
+  readonly birth: string;
+}
+export class LoginUserDto {
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsNotEmpty()
+  readonly password: string;
+}
