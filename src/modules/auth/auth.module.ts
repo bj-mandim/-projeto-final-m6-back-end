eb324c9c19b716170cca9ff62684ef68646add2e
Gabriel Werneck <gabrielwrnck@gmail.com>
2023-04-18 18:14:05 -0300

    Created Token Guard and Announcer Guard

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TokenStrategy } from './token.strategy';
 
 @Module({
+  imports: [PassportModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, TokenStrategy],
 })
 export class AuthModule {}
---
+++ src/modules/auth/self.guard.ts
@@ -0,0 +1,9 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+
+@Injectable()
+export class SelfGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    return request.params.id == request.userId;
+  }
+}
