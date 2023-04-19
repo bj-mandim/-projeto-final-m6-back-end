138c527ca52364d4a4d3b79d61cf3f77f2edd75c
Gabriel Werneck <gabrielwrnck@gmail.com>
2023-04-19 17:51:41 -0300

    Created route protection for cars

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TokenStrategy } from './token.strategy';
+import { CarsModule } from '../cars/cars.module';
 
 @Module({
-  imports: [PassportModule],
+  imports: [PassportModule, CarsModule],
   controllers: [AuthController],
   providers: [AuthService, TokenStrategy],
 })
